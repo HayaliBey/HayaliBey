b4666622b7e3a5f67fee0aae741e7189c5ab5d27
MONSTER <MONSTER@HAYL-DESKTOP->
2021-01-17 00:56:42 +0300

    Add project files.

---
+++ OdevCalısırken/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OdevCalısırken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            while (sayi <= 1000)
+            {
+                Console.WriteLine(sayi);
+                System.Threading.Thread.Sleep(1000);
+                sayi++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
