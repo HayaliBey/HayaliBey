a0372d1620debae711db3cb3731cf097e887500d
MONSTER <MONSTER@HAYL-DESKTOP->
2021-01-24 03:24:48 +0300

    23 Ocak 2021

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -12,6 +12,9 @@ namespace GenericsIntro
         {
             items = new T[0];
         }
+
+        public bool Length { get; internal set; }
+
         public void Add(T item)
         {
             T[] tempArray = items; //geçici dizi
---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Şubede internette ekranda görülen krediler
+            IKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DateBaseLoggerService();
+            ILoggerService fileLoggerService = new FileBaseLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager, new DateBaseLoggerService());
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
